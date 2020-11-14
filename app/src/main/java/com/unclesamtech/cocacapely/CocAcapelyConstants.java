814be71c6739a152d924687acc95f2f170cdcda6
Samuel Joel <31622538+UncleSamTech@users.noreply.github.com>
2020-11-14 07:19:52 +0000

    Updated CocAcapelyConstants
    update

--- app/src/main/java/com/unclesamtech/cocacapely/CocAcapelyConstants.java
+++ app/src/main/java/com/unclesamtech/cocacapely/CocAcapelyConstants.java
@@ -4,24 +4,31 @@ import android.view.View;
 import android.widget.Toast;
 
 public class CocAcapelyConstants {
-    private static final String MD5 =  "22:31:27:5E:2E:DE:AD:FB:23:79:1B:0B:13:01:C5:07";
+   // private static final String MD5 =  "22:31:27:5E:2E:DE:AD:FB:23:79:1B:0B:13:01:C5:07";
+    private static final String MD5 =  "";
     public static int RC_SIGN_IN_ID = 4247;
     public static int TOAST_LONG_LEN = Toast.LENGTH_LONG;
     public static int TOAST_SHORT_LEN = Toast.LENGTH_SHORT;
     public static int GALLERY_REQUEST_CODE = 3047;
-    private static String FB_APP_SECRET = "95c5309e6d6f79377f0e8e2ab3bb1f25";
+    //private static String FB_APP_SECRET = "95c5309e6d6f79377f0e8e2ab3bb1f25";
+    private static String FB_APP_SECRET = "";
     public static final String ERROR_MESS  = "Error as a result of : ";
     private static String FACEBOOK_APP_ID = "290580588929843";
     public static String PACKAGE_NAME = "com.unclesamtech.cocacapely";
-    private static final String DEEZ_APP_ID = "  428542";
-    private static final String DEEZ_SEC_KEY = "8d13582d3ae12c5d37bce898d54893da";
-
+    //private static final String DEEZ_APP_ID = "  428542";
+    private static final String DEEZ_APP_ID = " ";
+    private static final String DEEZ_SEC_KEY = "";
+    //private static final String DEEZ_SEC_KEY = "8d13582d3ae12c5d37bce898d54893da";
     public static int VISIBLE = View.VISIBLE;
     public static int INVISIBLE = View.INVISIBLE;
     public static int GONE = View.GONE;
     public static String EMPTY_VAL = "Empty Val";
-    private static String TWITTER_API_KEY = "7qbBhCP6n1dKN4uzY06Uo2H12";
-    private static String TWITTER_API_SECRET_KEY = "DdAKys0gNGPFHnKhLuX7mGZ3rGshgaVL9ZK42PefamdJEK49ZR";
-    private static final String SHA1  = "DB:BC:A8:C7:CD:92:85:CD:D2:14:76:1C:D8:B3:B5:1E:B0:FE:25:20";
-    private static final String SHA_256 = "AD:4A:45:2D:5D:D1:64:FC:D7:69:DA:56:5E:C6:7A:8F:DA:20:14:38:95:2E:A9:BE:A3:19:8C:BA:FB:AC:68:39";
+    private static String TWITTER_API_KEY = "";
+   // private static String TWITTER_API_KEY = "7qbBhCP6n1dKN4uzY06Uo2H12";
+    private static String TWITTER_API_SECRET_KEY = "";
+    //private static String TWITTER_API_SECRET_KEY = "DdAKys0gNGPFHnKhLuX7mGZ3rGshgaVL9ZK42PefamdJEK49ZR";
+    private static final String SHA1  = "";
+    //private static final String SHA1  = "DB:BC:A8:C7:CD:92:85:CD:D2:14:76:1C:D8:B3:B5:1E:B0:FE:25:20";
+    private static final String SHA_256 = "";
+    //private static final String SHA_256 = "AD:4A:45:2D:5D:D1:64:FC:D7:69:DA:56:5E:C6:7A:8F:DA:20:14:38:95:2E:A9:BE:A3:19:8C:BA:FB:AC:68:39";
 }
